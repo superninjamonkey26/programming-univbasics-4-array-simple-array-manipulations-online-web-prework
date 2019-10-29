f57efcb563910967454ba946130c49985ba44710
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-29 23:36:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,7 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array)
+def using_concat(array1, array2)
   array1 = ["raindrops on roses", "whiskers on kittens"]
   array2 = ["sports cars", "flatiron school"]
   array.concat
