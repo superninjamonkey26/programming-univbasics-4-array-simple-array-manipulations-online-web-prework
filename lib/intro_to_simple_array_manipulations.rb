be5c0480ecdaf5f6c32854e0f53ba3c9b55de088
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-30 00:29:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -59,6 +59,5 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  array = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  array.delete("Steven")
+  array.delete 
 end
